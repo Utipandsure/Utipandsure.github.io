ce73ed3844bc09305e3235370bc7f194ce71e3c1
Utip Cyril <utipandsure@yahoo.com>
2025-07-20 22:22:11 +0100

    Update script.js update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-  document.getElementById('nav-links').classList.toggle('show');
-  document.querySelector('.menu-toggle').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('show');
-});
 }
+
+document.querySelector('.menu-toggle').addEventListener('click', toggleMenu);
+
